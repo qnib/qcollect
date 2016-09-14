7d367b83990a888b9b9e33a3e9a1c87ec53b4b2f
Christian Kniep <christian@qnib.org>
2016-09-14 08:59:03 -0700

    adjust sed command to introduce version string

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 set -x
 
+cp main.go main.go.bkp
+GIT_TAG=$(git describe --abbrev=0 --tags)
 if [ -f /etc/os-release ];then
     . /etc/os-release
     if [ "X${ID}" != "Xalpine" ];then
       ID=Linux
+      sed -i'' -e "s/version =.*/version = \"${GIT_TAG}\"/" main.go
+    else
+      sed -i '' -e 's/version =.*/version = "0.6.4.1"/' main.go
     fi
 else
     ID=$(uname -s)
+    sed -i '' -e "s/version =.*/version = \"${GIT_TAG}\"/" main.go
 fi
 
 if [ ! -d ${GOPATH}/src/github.com/davecheney/profile ];then
@@ -21,9 +27,6 @@ go get github.com/pkg/errors github.com/stretchr/testify/assert
 #./cover.plt > ./resources/coverity/cover_$(git describe --abbrev=0 --tags).png
 #mv ./resources/coverity/cover_cur.out ./resources/coverity/cover_$(git describe --abbrev=0 --tags).out
 rm -f ./bin/qcollect_$(git describe --abbrev=0 --tags)_${ID}
-cp main.go main.go.bkp
-GIT_TAG=$(git describe --abbrev=0 --tags)
-sed -i '' -e "s/version =.*/version = \"${GIT_TAG}\"/" main.go
 go build -o ./bin/qcollect_${GIT_TAG}_${ID}
 mv main.go.bkp main.go
 rm -f ./bin/qcollect_latest_${ID}
