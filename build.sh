843a34006db71c5b89c479108205f4d4d9eae3b3
Christian Kniep <christian@qnib.org>
2016-10-17 17:48:47 +0200

    enhance the build-script

--- build.sh
+++ build.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-set -x
 
 cp main.go main.go.bkp
-GIT_TAG=$(git describe --abbrev=0 --tags)
+GIT_ORG_TAG=$(git describe --abbrev=0 --tags)
+git describe --exact-match --abbrev=0 > /dev/null
+if [ $? -ne 0 ];then
+    GIT_TAG="${GIT_ORG_TAG}-dirty"
+    BC_CMD=$(which bc)
+    if [ $? -ne 0 ];then
+        echo "!! Need bc command to calculate the number of commits since the latest tag -> proceed without..."
+    else
+        ## commit since tags
+        CNT_ALL=$(git log --oneline |wc -l)
+        CNT_COMMITS=$(echo "${CNT_ALL}-$(git log --oneline ${GIT_ORG_TAG} |wc -l)" |bc)
+        GIT_TAG="${GIT_TAG}-${CNT_COMMITS}"
+    fi
+fi
+
 if [ -f /etc/os-release ];then
     . /etc/os-release
     if [ "X${ID}" != "Xalpine" ];then
