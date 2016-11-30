76cc04c424d14c14be0ce07d3564d6b424ae8757
Christian Kniep <christian@qnib.org>
2016-11-30 21:18:47 +0100

    make test.sh even greater

--- test.sh
+++ test.sh
@@ -8,10 +8,12 @@ govendor sync
 if [ ! -d resources/coverity ];then
     mkdir -p resources/coverity
 fi
-go test -coverprofile=coverage.out
-for x in $(find . -maxdepth 1 -type d |egrep -v "(\.$|\.git|vendor|bin|resources)");do
-    go test -coverprofile=resources/coverity/${x}.out ${x} >>/dev/null
+go test -coverprofile=coverage.out >/dev/null
+COVER_OUTS="coverage.out"
+for x in $(find . -maxdepth 1 -type d |egrep -v "(\.$|\.git|vendor|bin|resources|deploy)");do
+    go test -coverprofile=resources/coverity/${x}.out ${x} >/dev/null
+    COVER_OUTS="${COVER_OUTS} resources/coverity/${x}.out"
 done
-coveraggregator -o resources/coverity/coverage-all.out $(find . -name coverage.out |egrep -v "(\.$|\.git|vendor|bin)") >>/dev/null
+coveraggregator -o resources/coverity/coverage-all.out ${COVER_OUTS} >/dev/null
 go tool cover -func=resources/coverity/coverage-all.out |tee ./coverage-all.out
 go tool cover -html=resources/coverity/coverage-all.out -o resources/coverity/all.html
